9bd990f987f1eea66130122053be1cc53c9c64b8
ConnectVerse <connectverse@Pranavs-Mac-mini.local>
2024-09-28 04:19:21 +0530

    commit

--- src/Home.js
+++ src/Home.js
@@ -5,6 +5,10 @@ import Courses from './Courses';
 import coin from './assets/coin.png'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import courses from './assets/courses.svg'
+import job from './assets/job.svg'
+import friend from './assets/friend.svg'
+import coin_airdrop from './assets/coin_airdrop.svg'
 
 function Home() {
 
@@ -12,21 +16,7 @@ function Home() {
     const notify = () => toast("Coming Soon !");
   return (
     <div>
-<Nav fill variant="tabs" defaultActiveKey="/home" style={{color:'white'}}>
-      <Nav.Item>
-        <Nav.Link href="/home">Courses</Nav.Link>
-      </Nav.Item>
-     
-      <Nav.Item>
-        <Nav.Link onClick={notify}>Jobs</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link onClick={notify}>Friends</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link onClick={notify}>Airdrop</Nav.Link>
-      </Nav.Item>
-    </Nav>
+
     <br></br><br></br><br></br>
     <center><h2 style={{color:'white'}}><img style={{width:'5em'}} src={coin}></img>{localStorage.getItem('coins')?localStorage.getItem('coins'):0}</h2></center>
     <br></br>
@@ -42,6 +32,33 @@ pauseOnHover
 theme="dark"
 
 />
+<Nav fill variant="tabs" defaultActiveKey="/home" style={{ color: 'white', position: 'fixed', bottom: 0, width: '100%', zIndex: 1000,backgroundColor:'black' }}>
+      <Nav.Item>
+        <Nav.Link href="/home">
+        <img style={{width:'40px'}}src={courses}></img>
+        <br/>
+        Courses</Nav.Link>
+      </Nav.Item>
+     
+      <Nav.Item>
+        <Nav.Link onClick={notify}>
+        <img style={{width:'40px'}}src={job}></img>
+        <br/>
+          Jobs</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link onClick={notify}>
+        <img style={{width:'40px'}}src={friend}></img>
+        <br/>
+          Friends</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link onClick={notify}>
+        <img style={{width:'40px'}}src={coin_airdrop}></img>
+        <br/>
+        Airdrop</Nav.Link>
+      </Nav.Item>
+    </Nav>
     </div>
  
   )
